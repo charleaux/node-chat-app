deea1ad3fb54d768bce3af9f3bd0c3e9786ff0fb
Charles Torry <ctorry@gmail.com>
2018-07-30 16:31:51 -0400

    added socketio

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 var app = express();
-
-
-// console.log(__dirname + '/../public');
-// console.log(publicPath);
+var server = http.createServer(app);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
