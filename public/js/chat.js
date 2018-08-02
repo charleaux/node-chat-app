946f0f241a879924177979cb4520a5b6b4f6c463
Charles Torry <ctorry@gmail.com>
2018-08-02 09:22:24 -0400

    added the users list

--- public/js/chat.js
+++ public/js/chat.js
@@ -32,6 +32,15 @@ socket.on('disconnect', function () {
     console.log('disconnected from server') ;
 });
 
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
+});
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
